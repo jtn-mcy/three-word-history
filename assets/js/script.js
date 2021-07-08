9b0b644b8d1383e8dc6a9ff879c9750520c33aa1
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-07 17:15:11 -0700

    add obtainArrays function, needs conditionals

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,19 +25,33 @@ var newsApiURL7toNow = newsApiBase.concat('qInTitle=', userInput, pageSize, sort
 
 
 
-//fetching from newsAPI, obtain results and dates
+//fetching from newsAPI, obtain array of article objects to extract dates and headlines/data from
 async function newsAPI (url) {
-    console.log(url);
+    // console.log(url);
     const response = await fetch(url);
-    console.log(response);
-    const data_1 = await response.json();
-    console.log(data_1);
-    console.log('Total results: ', data_1['totalResults']);
-    return data_1['articles'];
+    // console.log(response);
+    const data_newsAPI = await response.json();
+    // console.log(data_newsAPI);
+    console.log('Total results: ', data_newsAPI['totalResults']);
+    // console.log(data_newsAPI['articles'])
+    return data_newsAPI['articles'];
 }
 
-var articles = newsAPI(newsApiURL14to7);
-console.log(articles);
+var arrayDate = []; // dates
+var arrayHeadlineCount = []; //headlines count per date
 
-console.log('newsApiURL14to7 ', newsApiURL14to7);
-console.log('newsApiURL7toNow ', newsApiURL7toNow)
+async function obtainArrays () {
+    const articles14to7 = await newsAPI(newsApiURL14to7);
+    // console.log(articles14to7)
+    const articles7to0 = await newsAPI(newsApiURL7toNow);
+    var combinedArticles = articles14to7.concat(articles7to0);
+    console.log('Total amount of articles: ', combinedArticles.length)
+    for (i=0; i<combinedArticles.length; i++) {
+        arrayDate.push(combinedArticles[i]['publishedAt'].substr(0,9))
+    }
+}
+
+obtainArrays()
+
+// console.log('newsApiURL14to7 ', newsApiURL14to7);
+// console.log('newsApiURL7toNow ', newsApiURL7toNow)
